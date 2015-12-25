922f4df3193ad6a49498aa7c3eb112a5e2d4dd82
T_yun <18144200589@163.com>
2015-12-25 19:47:56 +0800

    完成功能ease

---
+++ XMYAnimationSimple/EasingViewController.swift
@@ -0,0 +1,64 @@
+//
+//  EasingViewController.swift
+//  XMYAnimationSimple
+//
+//  Created by T_yun on 15/12/25.
+//  Copyright © 2015年 T_yun. All rights reserved.
+//
+
+import UIKit
+
+class EasingViewController: UIViewController {
+
+    @IBOutlet weak var blueSquare: UIView!
+    @IBOutlet weak var redSquare: UIView!
+    @IBOutlet weak var greenSquare: UIView!
+    @IBOutlet weak var yellowSquare: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        UIView.animateWithDuration(1, animations: {
+        
+            self.blueSquare.center.x = self.view.bounds.width - self.blueSquare.center.x
+        })
+        
+        UIView.animateWithDuration(1, delay: 0, options: .CurveEaseIn, animations: {
+            
+            self.redSquare.center.x = self.view.bounds.width - self.redSquare.center.x
+            }, completion: nil)
+        
+        UIView.animateWithDuration(1, delay: 0, options: .CurveEaseOut, animations: {
+            
+            self.greenSquare.center.x = self.view.bounds.width - self.greenSquare.center.x
+            }, completion: nil)
+        
+        UIView.animateWithDuration(1, delay: 0, options: .CurveEaseInOut, animations: {
+            
+            self.yellowSquare.center.x = self.view.bounds.width - self.yellowSquare.center.x
+            }, completion: nil)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
