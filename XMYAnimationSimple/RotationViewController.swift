0d177fa3b63ce4671d1313ef019ce2f72039741d
T_yun <18144200589@163.com>
2015-12-25 16:48:36 +0800

    完成了Rotation功能 放入相应tag中

--- XMYAnimationSimple/RotationViewController.swift
+++ XMYAnimationSimple/RotationViewController.swift
@@ -10,12 +10,28 @@ import UIKit
 
 class RotationViewController: UIViewController {
 
+    @IBOutlet weak var wheel: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        self.spin()
+    }
+    
+    func spin() {
+        UIView.animateWithDuration(1, delay: 0, options: .CurveLinear, animations: {
+            
+            self.wheel.transform = CGAffineTransformRotate(self.wheel.transform, CGFloat(M_PI))
+            }) { (finished) -> Void in
+                self.spin()
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
